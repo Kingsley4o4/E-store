19f0824a0503491594721172662eeec39061993b
Kingsley <olorunfemiolakunle44@gmail.com>
2026-03-11 11:35:56 +0100

    LatestTest

--- E-store/src/App.jsx
+++ E-store/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   return (
     <>
       <h1 className='text-3xl font-bold text-red-500'>Hello World</h1>
-      <h2 className='text-2xl font-black flex justify-center'>new change</h2>
-      <h3>hello</h3>
+      <div className='w-2xl h-14 bg-blue-700 flex items-center justify-center'>
+        <h5>hello</h5>
+      </div>
     </>
   )
 }
