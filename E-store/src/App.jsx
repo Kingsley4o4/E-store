27faa808efd81a5498ea00505c41dd134895730e
Kingsley <olorunfemiolakunle44@gmail.com>
2026-03-11 10:51:41 +0100

    chang for test

---
+++ E-store/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-red-500'>Hello World</h1>
+      <h2 className='text-2xl font-black flex justify-center'>new change</h2>
+    </>
+  )
+}
+
+export default App
