4562bb30f7722b3c83c7bd80f0fb978fd71c40c3
Kingsley <olorunfemiolakunle44@gmail.com>
2026-03-11 11:09:25 +0100

    test

--- E-store/src/App.jsx
+++ E-store/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <h1 className='text-3xl font-bold text-red-500'>Hello World</h1>
       <h2 className='text-2xl font-black flex justify-center'>new change</h2>
+      <h3>hello</h3>
     </>
   )
 }
